4981f94147f2007dafb6fab625cdf53614f74d38
Marcin <nmarcin92@gmail.com>
2014-04-01 19:00:58 +0200

    initial commit

---
+++ HibernateTutorial/src/main/java/pl/edu/agh/bd2/tutorial/persiscence/HibernateUtil.java
@@ -0,0 +1,32 @@
+package main.java.pl.edu.agh.bd2.tutorial.persiscence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+	
+	static {
+		Configuration configuration = new Configuration();
+	    configuration.configure("main/resources/pl/edu/agh/bd2/tutorial/hibernate.cfg.xml");
+	    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+	    	            configuration.getProperties()).build();
+	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
